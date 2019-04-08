ba59be9a7448ead28291430baf394a01204b661e
kdpandey98 <48492047+kdpandey98@users.noreply.github.com>
2019-04-08 16:13:45 -0400

    Fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+# program Fizzbuzz
+# Kiran Pandey April 8, 2019
+
+for num in range(1,100):
+    if not num % 15:
+        print ("Fizzbuzz")
+    elif not num % 3 :
+        print ("Fizz")
+    elif not num % 5 :
+        print ('Buzz')
+    else:
+        print (num)
+    
+  
+
