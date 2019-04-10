b04e69664b7961c0e3ec15cbd9862097b74dd3b5
kdpandey98 <48492047+kdpandey98@users.noreply.github.com>
2019-04-10 16:14:28 -0400

    Add files via upload

--- Eliza_simple.py
+++ Eliza_simple.py
@@ -3,14 +3,12 @@
 """ Eliza, a thearapist,
     is an interactive chat-bot that aks the user about a problem,
     evaluates the user response and
-    interacts in a caring way
+    interacts in a caring way (response == parrot)
 """
 
 # prompt the user about a problem
 def ask(prompt):
-    response = ask("What brings you in today? ")
-    user_resp = input('Enter your response here or Q to quit: ')
-    return user_resp
+    pass
 
 #interact in a caring way
 def interact (user_resp):
@@ -25,11 +23,9 @@ print ('Good day. I am Eliza. Your therapist for today. What brings you in today
 
 # loop through until user wants to quit
 while not(done):
-    user_resp = input('Enter your response here or Q to quit: ')
+    user_resp = input('Enter your response here or "Q" to quit: ')
     if user_resp.upper() in exit:
+        print ('Thank you for visiting. Have a good rest of the day.')
         done = True
     else:
         interact(user_resp)
-
-
-
