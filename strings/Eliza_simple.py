e3f6fea179bfddf6130b858c7455d14252b44b4a
kdpandey98 <48492047+kdpandey98@users.noreply.github.com>
2019-04-11 10:16:40 -0400

    updated with comments

--- strings/Eliza_simple.py
+++ strings/Eliza_simple.py
@@ -3,29 +3,27 @@
 """ Eliza, a thearapist,
     is an interactive chat-bot that aks the user about a problem,
     evaluates the user response and
-    interacts in a caring way (response == parrot)
+    interacts in a caring way (response is unchanged == user response )
 """
 
-# prompt the user about a problem
-def ask(prompt):
-    pass
-
 #interact in a caring way
-def interact (user_resp):
-    print (user_resp)
+def get_response (user_resp):
+    ''' function determines Eliza's response with replacement words. '''
+    return(user_resp)
 
 #executable program starts here
 
 # initialize
 done = False
-exit = ['Q', 'I AM FEELING GREAT']
+exit = ['q', 'i am feeling great']      # condition to exit our of program
 print ('Good day. I am Eliza. Your therapist for today. What brings you in today? ')
 
 # loop through until user wants to quit
 while not(done):
-    user_resp = input('Enter your response here or "Q" to quit: ')
-    if user_resp.upper() in exit:
-        print ('Thank you for visiting. Have a good rest of the day.')
+    user_says = input('Enter your response here or "Q" to quit: ').lower()
+    print ('user: ',user_says)
+    if user_says.lower() != 'q':       # check if user wants to quit
+        print ('Eliza:', get_response(user_says))
+    if user_says.lower() in exit:       # check if valid exit conditions update while loop condition
         done = True
-    else:
-        interact(user_resp)
+        print ('Eliza: Thank you for stopping by. See you next time.')
